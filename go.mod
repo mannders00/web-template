c5390c2dacbeb90b7b06cd43dc79cade4b5156e7
matta9001 <matta9001@gmail.com>
2023-07-31 20:44:45 -0500

    Finished adding db

--- go.mod
+++ go.mod
@@ -1,22 +1,16 @@
-module github.com/matta9001/primehealthai
+module github.com/matta9001/pillars
 
-go 1.19
+go 1.20
 
 require (
-	github.com/gorilla/mux v1.8.0
-	github.com/joho/godotenv v1.5.1
-	github.com/mattn/go-sqlite3 v1.14.16
-	github.com/ory/client-go v1.1.27
+	github.com/mattn/go-sqlite3 v1.14.17
+	github.com/ory/client-go v1.1.41
 )
 
-require github.com/gorilla/securecookie v1.1.1 // indirect
-
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/gorilla/sessions v1.2.1
-	github.com/sashabaranov/go-openai v1.11.2
-	golang.org/x/net v0.9.0 // indirect
-	golang.org/x/oauth2 v0.7.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.12.0 // indirect
+	golang.org/x/oauth2 v0.10.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,26 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"embed"
+	"encoding/json"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
 	"text/template"
+
+	_ "github.com/mattn/go-sqlite3"
+	ory "github.com/ory/client-go"
 )
 
 //go:embed public/*
 var publicFS embed.FS
 
+type App struct {
+	ory *ory.APIClient
+	db  *sql.DB
+}
+
 func main() {
 
-	http.Handle("/public/", publicHandler())
-	http.HandleFunc("/", indexHandler)
+	// DB Initialization
+	newDB, err := sql.Open("sqlite3", "./db.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer newDB.Close()
 
-	err := http.ListenAndServe(":8080", nil)
+	statement, err := newDB.Prepare("CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, active INTEGER, streak TEXT)")
+	_, err = statement.Exec()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Ory Authentication
+	proxyPort := "4000"
+	c := ory.NewConfiguration()
+	c.Servers = ory.ServerConfigurations{{URL: fmt.Sprintf("http://localhost:%s/.ory", proxyPort)}}
+	app := &App{
+		ory: ory.NewAPIClient(c),
+		db:  newDB,
+	}
+
+	// HTTP Server
+	mux := http.NewServeMux()
+	mux.Handle("/", app.sessionMiddleware(app.getIndexHandler()))
+	mux.Handle("/public/", publicHandler())
+	mux.HandleFunc("/clicked", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			postClickHandler(w, r)
+		}
+	})
+	mux.HandleFunc("/test", getTestHandler)
+
+	err = http.ListenAndServe(":3000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func publicHandler() http.Handler {
@@ -31,10 +72,38 @@ func publicHandler() http.Handler {
 	return http.StripPrefix("/public/", http.FileServer(http.FS(httpFS)))
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func postClickHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+		<button hx-post="/clicked" hx-swap="outerHTML" class="btn btn-primary">fuck you</button>
+	`
+	_, err := fmt.Fprintf(w, html)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getTestHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFS(publicFS, "public/templates/header.tmpl", "public/html/index.html"))
 	err := tmpl.ExecuteTemplate(w, "index.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func (app *App) getIndexHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFS(publicFS, "public/templates/header.tmpl", "public/html/index.html"))
+		session := getSession(r.Context())
+		fmt.Println(session.Id)
+		sessionJSON, err := json.Marshal(session)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.ExecuteTemplate(w, "index.html", string(sessionJSON))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
